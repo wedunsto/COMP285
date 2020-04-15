043cd3a961c9d83da0ea96d1086d34f50ef5cee6
wedunsto <wedunsto@aggies.ncat.edu>
2020-04-15 18:06:43 -0400

    .

--- 6.cpp
+++ 6.cpp
@@ -1,98 +1,79 @@
-// A C++ program for Dijkstra's single source shortest path algorithm. 
-// The program is for adjacency matrix representation of the graph 
+#include<iostream>
+using namespace std;
+ 
+#include<bits/stdc++.h>
 
-#include <limits.h> 
-#include <stdio.h> 
+#define Size 6
 
-// Number of vertices in the graph 
-#define V 6
+int stoi1(string values){
+	int result = 0;
+	stringstream temp(values);
+	temp >> result;
+	return result;
+}
 
-// A utility function to find the vertex with minimum distance value, from 
-// the set of vertices not yet included in shortest path tree 
-int minDistance(int dist[], bool sptSet[]) 
+int SmallestPath(int weight[], bool Selected[]){//Needs fixing
+	int path = INT_MAX, nextInPath; 
+	for (int i=0; i<Size; i++) 
+		if (Selected[i] == false && weight[i] <= path) 
+			path = weight[i], nextInPath = i; 
+	return nextInPath; 
+}
+
+int OutPut(int weight[]) {
+	ofstream outputToFile;
+	outputToFile.open("results.txt");
+	char temp = 'B';	
+	for(int i = 1; i<Size; i++){
+		outputToFile<<"Shortest path from A to: "<<temp<<" is: "<<weight[i]<<endl;
+		temp = temp + 1;
+	}
+	outputToFile.close();
+}
+
+void Solution(int graph[Size][Size], int currentNode) 
 { 
-	// Initialize min value 
-	int min = INT_MAX, min_index; 
+	char temp = 'A';
+	char path[Size];
+	int weight[Size];
+	bool Selected[Size];
+	for(int i=0;i<Size;i++){
+		path[i] = temp;
+		temp = temp + 1; 
+	}
 
-	for (int v = 0; v < V; v++) 
-		if (sptSet[v] == false && dist[v] <= min) 
-			min = dist[v], min_index = v; 
-
-	return min_index; 
-} 
-
-// A utility function to print the constructed distance array 
-int printSolution(int dist[]) 
-{ 
-	printf("Vertex \t\t Distance from Source\n"); 
-	for (int i = 0; i < V; i++) 
-		printf("%d \t\t %d\n", i, dist[i]); 
-} 
-
-// Function that implements Dijkstra's single source shortest path algorithm 
-// for a graph represented using adjacency matrix representation 
-void dijkstra(int graph[V][V], int src) 
-{ 
-	int dist[V]; // The output array. dist[i] will hold the shortest 
-	// distance from src to i 
-
-	bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest 
-	// path tree or shortest distance from src to i is finalized 
-
-	// Initialize all distances as INFINITE and stpSet[] as false 
-	for (int i = 0; i < V; i++) 
-		dist[i] = INT_MAX, sptSet[i] = false; 
-
-	// Distance of source vertex from itself is always 0 
-	dist[src] = 0; 
-
-	// Find shortest path for all vertices 
-	for (int count = 0; count < V - 1; count++) { 
-		// Pick the minimum distance vertex from the set of vertices not 
-		// yet processed. u is always equal to src in the first iteration. 
-		int u = minDistance(dist, sptSet); 
-
-		// Mark the picked vertex as processed 
-		sptSet[u] = true; 
-
-		// Update dist value of the adjacent vertices of the picked vertex. 
-		for (int v = 0; v < V; v++) 
-
-			// Update dist[v] only if is not in sptSet, there is an edge from 
-			// u to v, and total weight of path from src to v through u is 
-			// smaller than current value of dist[v] 
-			if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
-				&& dist[u] + graph[u][v] < dist[v]) 
-				dist[v] = dist[u] + graph[u][v]; 
+	for (int i=0; i < Size; i++) 
+		weight[i] = INT_MAX, Selected[i] = false; 
+	weight[currentNode] = 0; 
+	for (int i=0; i < Size - 1; i++) { 
+		int newlyFound = SmallestPath(weight, Selected);
+		Selected[newlyFound] = true; 
+		for (int i=0; i < Size; i++){
+			if (Selected[i] != true && (graph[newlyFound][i] && weight[newlyFound] != INT_MAX) && weight[newlyFound] + graph[newlyFound][i] < weight[i]){
+				weight[i] = weight[newlyFound] + graph[newlyFound][i];
+			}
+		}
 	} 
-
-	// print the constructed distance array 
-	printSolution(dist); 
+	OutPut(weight);
 } 
 
-// driver program to test above function 
-int main() 
-{ 
-	/* Let us create the example graph discussed above */
-	int graph[V][V] = {{0,0,13,0,16,8},
-			   {0,0,0,6,0,10},
-			   {13,0,0,14,0,11},
-			   {0,6,14,0,5,17},
-			   {16,0,0,5,0,7},
-			   {8,10,11,17,7,0}};
-
-	/*int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
-						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 }, 
-						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 }, 
-						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 }, 
-						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 }, 
-						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 }, 
-						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 }, 
-						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 }, 
-						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } }; */
-
-	dijkstra(graph, 0); 
-
-	return 0; 
-} 
-
+int main(int argc, char* argv[]){
+	fstream file;
+	string fileName;
+	string values;
+	if(argc>1){
+		fileName = argv[1];
+		file.open(fileName.c_str());
+		file>>values;
+		int graph[Size][Size];
+		for(int i=0; i<Size; i++){
+			for(int j=0; j<Size; j++){
+				file >> values;
+				graph[i][j] = stoi1(values);
+			}
+		}
+		Solution(graph, 0); 
+	}
+	else cout<<"Please enter the filename"<<endl;
+	return 0;
+}
---
+++ test1.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <list>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+class Edge
+{
+public:
+	char source_;
+	char dest_;
+	int weight_;
+
+	Edge(char source, char dest, int weight) : source_(source), dest_(dest), weight_(weight) {}
+};
+
+class NodeDist
+{
+public:
+	char node_;
+	int dist_;
+	char parent_;
+	NodeDist(char node, int dist, char parent) : node_(node), dist_(dist), parent_(parent) {}
+};
+
+bool operator<(const NodeDist &e1, const NodeDist &e2)
+{
+	return e1.dist_ > e2.dist_;
+}
+
+class Graph
+{
+
+public:
+	Graph(int num_nodes) { num_nodes_ = num_nodes; }
+	std::map<char, std::list<Edge> > adj_list;
+	int num_nodes_;
+
+	void addEdge(Edge e)
+	{
+		adj_list[e.source_].push_back(e);
+	}
+	void printGraph()
+	{
+		std::map<char, std::list<Edge> >::iterator it = adj_list.begin();
+		while (it != adj_list.end())
+		{
+			printEdgesOfVertex(it);
+			it++;
+		}
+	}
+	void printEdgesOfVertex(const std::map<char, std::list<Edge> >::iterator &it)
+	{
+		std::cout << it->first << "-->";
+		for (std::_List_iterator<Edge> list_it = it->second.begin(); list_it != it->second.end(); ++list_it)
+			std::cout << ' ' << list_it->dest_ << "{" << list_it->weight_ << "}";
+		std::cout << std::endl;
+	}
+
+	void printEdge(Edge e)
+	{
+		std::cout << e.source_ << "-->" << e.dest_ << "{" << e.weight_ << "}" << std::endl;
+	}
+
+	void dijkstraFromA()
+	{
+		std::priority_queue<NodeDist> pq;
+		std::set<char> visited;
+		std::map<char, std::string> pathMap;
+		std::map<char, int> pathCost;
+		visited.insert('A');
+		pathMap.insert({'A', "A"});
+		pathCost.insert({'A', 0});
+		//cout << "hi" << std::endl;
+		addNodesToPq(pq, 'A', 0);
+		std::string path;
+
+		while (!pq.empty() && visited.size() < num_nodes_)
+		{
+			//cout << "hi";
+			NodeDist nd = pq.top();
+			//printEdge(e);
+			pq.pop();
+			if (visited.find(nd.node_) == visited.end())
+			{
+				pathMap.insert({nd.node_, pathMap[nd.parent_] + nd.node_});
+				pathCost.insert({nd.node_, nd.dist_});
+				addNodesToPq(pq, nd.node_, nd.dist_);
+				visited.insert(nd.node_);
+			}
+		}
+		writePathAndCost(pathMap, pathCost);
+	}
+
+	void writePathAndCost(std::map<char, std::string> &pathMap, std::map<char, int> &pathCost)
+	{
+		ofstream myfile;
+		myfile.open("result.txt");
+		for (int i = 1; i < num_nodes_; i++)
+		{
+			char dest = 'A' + i;
+			myfile << "Shortest path from A to " << dest << " is: " << pathCost.find(dest)->second << std::endl;
+			myfile << "Path taken: " << pathMap.find(dest)->second << std::endl;
+		}
+		myfile.close();
+	}
+
+	void addNodesToPq(std::priority_queue<NodeDist> &pq, char source, int sourceDist)
+	{
+		//cout << "hi";
+		if (adj_list.find(source) != adj_list.end())
+		{
+			for (std::_List_iterator<Edge> list_it = adj_list.find(source)->second.begin(); list_it != adj_list.find(source)->second.end(); ++list_it)
+			{
+
+				pq.push(NodeDist(list_it->dest_, sourceDist + list_it->weight_, source));
+			}
+		}
+	}
+};
+
+int main()
+{
+	ifstream myReadFile;
+	myReadFile.open("ex.txt");
+	std::string next_line;
+	int num_nodes = 0;
+	if (std::getline(myReadFile, next_line))
+	{
+		std::istringstream iss(next_line);
+		iss >> num_nodes;
+	}
+
+	if (num_nodes == 0)
+		return 0;
+
+	char source = 'A';
+	Graph g(num_nodes);
+	while (std::getline(myReadFile, next_line))
+	{
+		std::istringstream iss(next_line);
+		int temp;
+		char dest = source;
+		while (iss >> temp)
+		{
+
+			dest++;
+			if (temp != 0)
+			{
+				g.addEdge(Edge(source, dest, temp));
+				g.addEdge(Edge(dest, source, temp));
+			}
+		}
+		source++;
+	}
+
+	myReadFile.close();
+	g.printGraph();
+	g.dijkstraFromA();
+	return 0;
+}
